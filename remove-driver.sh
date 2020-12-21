5c84179396329cacb0827b415ea450ef100c16fe
morrownr <morrownr@gmail.com>
2020-12-20 22:14:27 -0600

    minor updates

--- remove-driver.sh
+++ remove-driver.sh
@@ -7,7 +7,7 @@ OPTIONS_FILE=8821au.conf
 SCRIPT_NAME=remove-driver.sh
 
 if [[ $EUID -ne 0 ]]; then
-	echo "You must run this removal script with superuser (root) priviliges."
+	echo "You must run this removal script with superuser (root) privileges."
 	echo "Try \"sudo ./${SCRIPT_NAME}\""
 	exit 1
 fi
@@ -16,11 +16,10 @@ dkms remove ${DRV_NAME}/${DRV_VERSION} --all
 RESULT=$?
 
 if [[ "$RESULT" != "0" ]]; then
-	echo "An error occurred while running ${SCRIPT_NAME} : $RESULT"
+	echo "An error occurred while running ${SCRIPT_NAME} : ${RESULT}"
 	exit $RESULT
 else
 	rm -f /etc/modprobe.d/${OPTIONS_FILE}
 	rm -rf /usr/src/${DRV_NAME}-${DRV_VERSION}
-	echo "${SCRIPT_NAME} was successful."
-	exit 0
+	echo "The module has been removed successfully."
 fi
