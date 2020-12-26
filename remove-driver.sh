f25c9b074abd8b0bf5966227949636aa5089f806
morrownr <morrownr@gmail.com>
2020-12-25 19:28:43 -0600

    minor updates

--- remove-driver.sh
+++ remove-driver.sh
@@ -4,7 +4,7 @@ DRV_NAME="rtl8821au"
 DRV_VERSION="5.8.2.3"
 OPTIONS_FILE="8821au.conf"
 
-KRNL_VERSION=$(uname -r)
+KRNL_VERSION="$(uname -r)"
 SCRIPT_NAME="remove-driver.sh"
 
 if [[ $EUID -ne 0 ]]; then
